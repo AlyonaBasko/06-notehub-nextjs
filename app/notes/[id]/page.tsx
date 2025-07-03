f4fbe5c3866428c33bd0c8076af33e47a7ee79ac
alona <alenabask93@gmail.com>
2025-07-03 15:15:00 +0200

    update page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,17 +2,26 @@ import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
-export default async function NoteDetailsPage({ params: { id } }: { params: { id: string } }) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function NoteDetailsPage({ params }: PageProps) {
   const qc = new QueryClient();
-  const numId = Number(id);
+  const numId = Number(params.id);
+
   await qc.prefetchQuery({
     queryKey: ['note', numId],
     queryFn: () => fetchNoteById(numId),
-    staleTime: 1000 * 60, // 1 хвилина
+    staleTime: 1000 * 60,
   });
+
   return (
     <HydrationBoundary state={dehydrate(qc)}>
       <NoteDetailsClient id={numId} />
     </HydrationBoundary>
   );
 }
+
