4711c963db7041586601789979583e7cd50f1e91
alona <alenabask93@gmail.com>
2025-07-03 15:57:24 +0200

    update page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,18 +1,12 @@
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
-import type PageProps from 'next/types';
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function NoteDetailsPage({ params }: PageProps) {
+export default async function NoteDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+  const realParams = await params;
+  const id = Number(realParams.id);
+  
   const queryClient = new QueryClient();
-  const id = Number(params.id);
-
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
--- main.tsx
+++ main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -10,7 +9,6 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
